726e0fef3900791a8a827d459cffe7910d00ae0d
alanhugheslandregistry <alanhugheslandregistry@gmail.com>
2014-07-08 12:16:46 +0100

    Fixing tests

--- app_tests.py
+++ app_tests.py
@@ -4,15 +4,15 @@ import unittest
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        app.app.config['TESTING'] = True
-        self.app = app.app.test_client()
+        hello.app.config['TESTING'] = True
+        self.app = hello.app.test_client()
 
     #Nothing to put into the teardown for this testcase
     #def tearDown(self):
 
     def test_hello_world(self):
         rv = self.app.get('/')
-        assert 'Hello World' in rv.data
+        assert 'Hello World3' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
